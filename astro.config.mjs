e29c845f5b48667611fb9a72eada1f44d5b5e96a
Matej Bačo <matejbaco2000@gmail.com>
2024-12-28 22:35:46 +0100

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
