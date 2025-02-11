20badfc6a9812d6fcb0f4a0bef80a88989ae0709
Matej Bačo <matejbaco2000@gmail.com>
2025-02-11 15:30:19 +0100

    Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [tailwind(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
