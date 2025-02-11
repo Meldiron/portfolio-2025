ee050aa82fd952fbde28df2172fd59d832d61d7a
Matej Bačo <matejbaco2000@gmail.com>
2025-02-11 16:16:59 +0100

    Revert comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Appwrite
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
