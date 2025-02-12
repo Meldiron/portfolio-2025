562347c09866926591cf36c5157a586ab9c9dbd4
Matej Bačo <matejbaco2000@gmail.com>
2025-02-12 09:48:44 +0100

    Enable vercel SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
